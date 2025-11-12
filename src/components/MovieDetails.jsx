47b9be9d8117ebfd10a66342237588f3d62cdbec
mahmoudKSanad <sanadmahmoud50@gmail.com>
2025-11-12 20:17:40 +0200

    fix fetching data

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -79,7 +79,7 @@ export default function MovieDetails({
       async function fetchMovie() {
         setIsLoading(true)
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         )
         const data = await res.json()
         setMovie(data)
